8b13b08408e8912d2bf39930ee638ac5425e460e
sndnvaps <sndnvaps@gmail.com>
2016-09-15 17:44:20 +0800

    keymaster: rename keymaster.mdt -> keymaste.mdt
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- qcom-common/qcom-common-vendor-blobs-keymaster.mk
+++ qcom-common/qcom-common-vendor-blobs-keymaster.mk
@@ -22,4 +22,5 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/qcom-common/proprietary-keymaster/firmware/keymaster/keymaster.b01:system/vendor/firmware/keymaster/keymaster.b01 \
     vendor/xiaomi/qcom-common/proprietary-keymaster/firmware/keymaster/keymaster.b02:system/vendor/firmware/keymaster/keymaster.b02 \
     vendor/xiaomi/qcom-common/proprietary-keymaster/firmware/keymaster/keymaster.b03:system/vendor/firmware/keymaster/keymaster.b03 \
-    vendor/xiaomi/qcom-common/proprietary-keymaster/firmware/keymaster/keymaster.mdt:system/vendor/firmware/keymaster/keymaster.mdt 
+    vendor/xiaomi/qcom-common/proprietary-keymaster/firmware/keymaster/keymaster.mdt:system/vendor/firmware/keymaster/keymaster.mdt \
+   vendor/xiaomi/qcom-common/proprietary-keymaster/firmware/keymaster/keymaste.mdt:system/vendor/firmware/keymaster/keymaste.mdt 
