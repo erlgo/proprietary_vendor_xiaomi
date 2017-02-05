ee4c9f7e7b69cfefcaf8f38360666d87d20ea3c7
papjul <julien.papasian@gmail.com>
2017-02-05 21:11:49 +0100

    Remove unneeded blobs for aries

--- cancro/cancro-vendor-blobs.mk
+++
@@ -1,4 +0,0 @@
-# FM
-PRODUCT_COPY_FILES += \
-    vendor/xiaomi/cancro/proprietary/bin/fmconfig:system/bin/fmconfig \
-    vendor/xiaomi/cancro/proprietary/bin/fm_qsoc_patches:system/bin/fm_qsoc_patches
--- virgo/virgo-vendor-blobs.mk
+++
@@ -1,7 +0,0 @@
-
-# sensor
-PRODUCT_COPY_FILES += \
-    vendor/xiaomi/virgo/proprietary/bin/sensorextd:system/bin/sensorextd
-
-PRODUCT_COPY_FILES += \
-    vendor/xiaomi/virgo/proprietary/lib/hw/sensors.st_mve.so:system/lib/hw/sensors.st_mve.so
